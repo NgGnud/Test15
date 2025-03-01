4e24554e3785c6af98b5abf1eb86871aee5351b9
NgGnud <163308193+NgGnud@users.noreply.github.com>
2025-03-01 13:08:31 +0700

    hihi

---
+++ src/main/java/org/example/test15/Test15Application.java
@@ -0,0 +1,13 @@
+package org.example.test15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test15Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/test15/Test15ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.test15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
